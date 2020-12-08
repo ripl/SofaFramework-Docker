55c37fecab9df8f87d9b5d0d699935189993bf61
Arthur MacKeith <amackeith@uchicago.edu>
2020-12-07 23:12:44 -0600

    ready

--- launch_docker.sh
+++ launch_docker.sh
@@ -1,1 +1,1 @@
-docker run  -it --net=host --privileged -v 'pwd'/workdir:/home/sofauser -p 8888:8888  ripl/sofasoftrobots:latest
+x-docker run  -it --net=host --privileged -v `pwd`/workdir:/home/sofauser/workdir -p 8888:8888  ripl/sofasoftrobots-problem-illustration:latest
