327e24a8e4edbfa218f514de44bee4c94071f635
Arthur MacKeith <amackeith@uchicago.edu>
2020-12-08 22:45:47 -0600

    independent dockerfile

--- launch_docker.sh
+++ launch_docker.sh
@@ -1,1 +1,1 @@
-x-docker run  -it --net=host --privileged -v `pwd`/workdir:/home/sofauser/workdir -p 8888:8888  ripl/sofasoftrobots-problem-illustration:latest
+sudo x-docker run  --gpus all -it --net=host --privileged -v `pwd`/workdir:/home/sofauser/workdir -p 8888:8888  ripl/sofasoftrobots-problem-illustration:latest
