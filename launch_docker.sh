b86a7e91b2601f684e8b2f29837c90e5798cc891
Arthur MacKeith <amackeith@uchicago.edu>
2020-12-14 20:49:26 -0600

    small

--- launch_docker.sh
+++ launch_docker.sh
@@ -1,1 +1,1 @@
-x-docker run  --gpus all -it --net=host --privileged -v `pwd`/workdir:/home/sofauser/workdir -p 8888:8888  ripl/sofasoftrobots-python3:latest
+x-docker run   -it --net=host --privileged -v `pwd`/workdir:/home/sofauser/workdir -p 8888:8888  ripl/sofasoftrobots-python3:latest
