af530ba480fede4021990af0be78680e91fc377b
Arthur <amackeith@ttic.edu>
2021-01-01 22:40:31 -0600

    sm

--- no_gui_launch.sh
+++ no_gui_launch.sh
@@ -1,1 +1,1 @@
-docker run -it --network host -e DISPLAY=$DISPLAY --privileged -v `pwd`/workdir:/home/sofauser/workdir -p 8888:8888  ripl/sofasoftrobots-python3:latest
+docker run -it -v /tmp/.X11-unix:/tmp/.X11-unix:ro --network host -e DISPLAY=172.17.0.1:10.0 --privileged -v `pwd`/workdir:/home/sofauser/workdir -p 8888:8888  ripl/sofasoftrobots-python3:latest
