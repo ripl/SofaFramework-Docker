db2bb0877f6738104d95994d1438f329ab21e74b
Arthur <amackeith@ttic.edu>
2021-01-05 17:53:39 -0600

    small

--- rllaunch.sh
+++ rllaunch.sh
@@ -1,1 +1,3 @@
-x-docker run --gpus all -it --net=host --privileged -v `pwd`/../dl/dl:/pkgs/dl -v `pwd`/workdir:/home/sofauser/workdir -p 8888:8888  ripl/sofasoftrobots-python3:latest
+docker run --name sofa_train --rm  --gpus all -it --net=host --privileged -v `pwd`/../dl/dl:/pkgs/dl -v `pwd`/workdir:/home/sofauser/workdir -p 8888:8888  ripl/sofasoftrobots-python3:latest
+
+
---
+++ tensorboard.sh
@@ -0,0 +1,1 @@
+docker exec -it sofa_train tensorboard --logdir /pkgs/dl/examples/sofa_start/logs
