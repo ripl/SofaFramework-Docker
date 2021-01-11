b9c398fd7853d6d3618af56e161e92963466bb8b
Arthur MacKeith <amackeith@uchicago.edu>
2021-01-11 11:58:18 -0600

    cleanup

--- no_gui_launch.sh
+++
@@ -1,1 +0,0 @@
-docker run -it -v /tmp/.X11-unix:/tmp/.X11-unix:ro --network host -e DISPLAY=172.17.0.1:10.0 --privileged -v `pwd`/workdir:/home/sofauser/workdir -p 8888:8888  ripl/sofasoftrobots-python3:latest
