02cb480d2f9f839808e66ca600f70262cc3bf9a3
Arthur MacKeith <amackeith@uchicago.edu>
2021-01-22 19:33:30 -0600

    updates sceneClass for collision

--- workdir/simple_control_policy/simulation_wrapper.py
+++ workdir/simple_control_policy/simulation_wrapper.py
@@ -32,7 +32,7 @@ class scene_interface:
     """Scene_interface provides step and reset methods"""
     def __init__(self, env_id, design= np.array([[[0, 0]]]), dt = 0.001, max_steps=300,
                  meshFolder=os.path.dirname(os.path.abspath(__file__)) + '/mesh/',
-                 debug=False, record_episode=False, steps_per_action=3):
+                 debug=False, record_episode=False, steps_per_action=3, model_args={}):
         
         # it is 1x1x2 with a cavity in both positions
 
@@ -62,6 +62,9 @@ class scene_interface:
         self.sim_time = 0.0
         # the current step in the simulation
         self.current_step = 0
+
+        # any non defualt args for the scene
+        self.model_args = model_args
         
         self.debug = debug
         self.debug_line_numb = 0
@@ -75,7 +78,7 @@ class scene_interface:
         self.metadata = None
         self.spec = None
         self.reward_range = (-100000, 100000)
-        self.action_space = spaces.Box(low=0.0, high=7.0, shape=design.shape,
+        self.action_space = spaces.Box(low=0.0, high=99999.0, shape=design.shape,
                                        dtype=np.float32)
         self.observation_space = spaces.Box(low=2.0, high=2.35,
                                             shape=(1,), dtype=np.float32)
@@ -149,12 +152,12 @@ class scene_interface:
             # create the scene
             self.scene = sceneClass.SceneDefinition(self.root, design=self.design, dt=self.dt,
                               meshFolder=self.meshFolder,
-                              with_gui=self.record_episode, debug=self.debug)
+                              with_gui=self.record_episode, debug=self.debug, **self.model_args)
 
             # if we want to record the session we need to have a gui
-            if self.record_episode:
-                robot_position = np.array([0, 0, 0])
-                camera_position = np.array([0, 0, 10])
+            if self.record_episode or True:
+                robot_position = np.array([82, -50, 0])
+                camera_position = np.array([82, -350, 10])
                 position = list(camera_position)
                 direction = robot_position - camera_position
                 direction = list(direction / np.linalg.norm(direction))
@@ -225,6 +228,7 @@ class scene_interface:
         if np.isnan(np.sum(observation)):
             self.debug_output("found nan in observation "+str(time.time()),
                               filename="/home/sofauser/workdir/debug_nan.txt")
+            exit()
         np.clip(observation, self.observation_space.low,
                 self.observation_space.high, out=observation)
         
@@ -282,25 +286,28 @@ class scene_interface:
             prefix = str(other_info) + "_"
 
         Sofa.Simulation.updateVisual(self.root)
-        Sofa.Gui.GUIManager.SaveScreenshot(prefix + str(self.current_step)+"test.png")
+        Sofa.Gui.GUIManager.SaveScreenshot("SIM" + str(self.current_step)+ prefix + "test.png")
 
 
         
         
 #@profile
-def sim_run(a,design, i):
+def sim_run(a,design, i, mx):
     done = False
     total = 0
-    x = np.linspace(0,7, 105)
+    x = list(np.linspace(0,1, mx))
+    x.reverse()
+
     while not done:
         factor = a.current_step / a.max_steps
         act = 5 * np.cos(factor * np.pi * 2) * np.ones(design.shape)
-        action = act * 0  + x[-i]
-        obs2, reward, done, info = a.step(action, other_info="Sim" + str(i))
+        action = act * 0  + factor*1200
+        action[0,0,0] = factor*2000
+        obs2, reward, done, info = a.step(action, other_info= "_"+str(i) + "_" + str(np.mean(action)))
         total += reward
+
         print('previous action:', action, 'observation: ', a.observation,
               'reward:', reward, " total reward ", total)
-
     a.reset()
 
 def main():
@@ -309,13 +316,22 @@ def main():
     # it is 1x1x2 with a cavity in both positions
     #design = np.array([[[0, 0]]])
     design = np.array([[[ 0]]])
+    #a = scene_interface(0, design=design, dt=0.001, max_steps=200,
+    #                    meshFolder=meshpath, steps_per_action=1, record_episode=True,
+    #                    debug=False)
 
-    a = scene_interface(0, design=design, dt=0.001, max_steps=3,
-                        meshFolder=meshpath, steps_per_action=3, record_episode=True,
-                        debug=True)
+    # it is 1x1x2 with a cavity in both positions
+    design = np.array([[[0, 0, 0, 0, 0]]])
+    # this is where the example mesh is stored in the docker file.
+    meshpath = '/home/sofauser/workdir/simple_control_policy/quadruped/' #
+    a = scene_interface(0, design=design, dt=0.01, max_steps=400,
+                        meshFolder=meshpath, steps_per_action=1, record_episode=True,
+                        debug=False, model_args={'collision':True,'poissonRatio':'0.05',  "youngModulus":'70',
+                                                "totalMass":0.0035, 'constraint':"floor"})
 
-    for i in range(100):
-        sim_run(a, design, i)
+    mx = 10
+    for i in range(mx):
+        sim_run(a, design, i, mx)
 
 if __name__ == '__main__':
     main()
